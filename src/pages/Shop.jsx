9157c895158d970b4da58615d984b511a20be76e
salman <truesalman7@gmail.com>
2024-09-27 18:16:42 +0500

    navbar done

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-const Shop=()=>{
-    return(
-        <div>Shop</div>
-    )
-
-}
+import Layout from "../component/Layout/Layout";
+const Shop = () => {
+  return (
+    <>
+      <Layout>
+        <h2>SHop</h2>
+      </Layout>
+    </>
+  );
+};
 export default Shop;
