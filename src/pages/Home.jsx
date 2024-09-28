b545f2ae64e1ec84abc6ebdb73e62c9c761cef9b
salman <truesalman7@gmail.com>
2024-09-28 13:39:27 +0500

    home hero sectin done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import Layout from "../component/Layout/Layout";
+import HeroPage from "../component/Hero/Hero";
+
 const Home = () => {
-  return <Layout></Layout>;
+  return (
+    <>
+      <Layout>
+        <HeroPage />
+      </Layout>
+    </>
+  );
 };
+
 export default Home;
